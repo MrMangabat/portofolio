c10951b6427b49ce792143223c97d5650e14eebd
Jannik <mangabat93@gmail.com>
2025-03-03 16:22:20 +0100

    Re-adding agent_tailored_cover_letter with new structure

--- backend/aiml_models/agent_teams/agent_tailored_cover_letter/src/llm_model/common.py
+++
@@ -1,6 +0,0 @@
-from llm_agents import LLM_model
-from backend.services.service_cover_letter.src.config.config_low_level import GPT_API
-
-def get_llm_model():
-    llm_instance = LLM_model.LLMModel(openai_api_key=GPT_API, temperature=0.1)
-    return llm_instance.get_llm_model()
